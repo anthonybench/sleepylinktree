9b4ebc2d170258e31afd1c42158774d563af394f
iyep_halcyon <iyep@halcyon.ai>
2025-05-09 12:44:15 -0700

    refactored to avoid monolith middleman component, bento-grid now isolated

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
 "use client";
 import { useTheme } from "@/app/themes/switch-board";
-import Monolith from "./components/monolith";
-// import Header from "./components/header";
+import Header from "@/app/components/header";
+import BentoGrid from "@/app/components/bento-grid";
 
 export default function Home() {
-  const theme = useTheme();
+  const theme = useTheme().theme;
   return (
-    <>
-      <Monolith />
-    </>
+    <div className={`${theme}-bg-primary py-24 sm:py-32`}>
+      <div className="mx-auto max-w-2xl px-6 lg:max-w-7xl lg:px-8">
+        <Header />
+        <BentoGrid />
+      </div>
+    </div>
   );
 }
